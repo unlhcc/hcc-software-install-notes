72388d2ae1a357f983beadc4c81088549fde0bdd
Adam Caprez <acaprez2@unl.edu>
2020-09-16 16:52:54 -0500

    Build against PGI expat.

--- packages/udunits/2.2.26/compile.crane.pgi20.bash
+++ packages/udunits/2.2.26/compile.crane.pgi20.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 module purge
-module load compiler/pgi/20
-
-make clean && make distclean
-
+module load compiler/pgi/20 expat/2.2
+export PREFIX=/util/opt/udunits/2.2.26/pgi/20.4
+export LDFLAGS="-L${EXPAT}/lib"
 export PERL=''
-./configure --prefix=/util/opt/udunits/2.2.26/pgi/20.4
+
+make clean
+make distclean
+./configure --prefix=$PREFIX
 make
 make install
