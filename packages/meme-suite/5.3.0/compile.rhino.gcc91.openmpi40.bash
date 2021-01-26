4624e0fa86e39a216b411aa937e4fda2cb2ffc25
Natasha Pavlovikj <npavlovikj2@unl.edu>
2021-01-26 16:14:20 -0600

    Update compile instructions for meme-suite for large datasets

--- packages/meme-suite/5.3.0/compile.rhino.gcc91.openmpi40.bash
+++ packages/meme-suite/5.3.0/compile.rhino.gcc91.openmpi40.bash
@@ -3,6 +3,7 @@
 module load compiler/gcc/9.1 openmpi/4.0 perl/5.26 python/2.7 zlib/1.2
 
 ./configure --prefix=/util/opt/meme-suite/5.3.0/gcc/9.1.0/openmpi/4.0.1/ --enable-build-libxml2 --enable-build-libxslt --enable-opt -with-mpidir=/util/opt/openmpi/4.0/gcc/9.1/
-make -j8
+# https://sourceforge.net/p/mummer/discussion/451664/thread/59ad55aa/#bf1b, for larger datasets 
+make CPPFLAGS="-O3 -DSIXTYFOURBITS" -j8
 make test -j8
 make install
