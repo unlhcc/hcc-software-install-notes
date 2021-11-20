0435e6c4504c63f631b7fb38415f22bcb1a07bae
Adam Caprez <acaprez2@unl.edu>
2021-11-20 08:49:47 -0600

    Add compile scripts for openblas.

---
+++ packages/openblas/0.3.18/compile.crane.gcc10.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module purge
+module load compiler/gcc/10
+PREFIX=/util/opt/openblas/0.3.18/gcc/10
+
+make clean
+make PREFIX=$PREFIX TARGET=SANDYBRIDGE DYNAMIC_ARCH=1 USE_OPENMP=1 NUM_THREADS=128 -j 4
+make PREFIX=$PREFIX TARGET=SANDYBRIDGE DYNAMIC_ARCH=1 USE_OPENMP=1 NUM_THREADS=128 -j 1 install
+pushd $PREFIX && ln -s libopenblas.so libblas.so
