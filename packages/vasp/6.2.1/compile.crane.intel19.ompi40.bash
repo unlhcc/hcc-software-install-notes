559499ccf3c50238019589d9de6fbd9c4d519c0d
Adam Caprez <acaprez2@unl.edu>
2022-02-01 19:45:42 -0600

    Add missing modules.

--- packages/vasp/6.2.1/compile.crane.intel19.ompi40.bash
+++ packages/vasp/6.2.1/compile.crane.intel19.ompi40.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+module purge
+module load compiler/intel/19 openmpi/4.0 intel-mkl/19
+
 rm -f makefile.include && cp makefile.intel19.ompi40.include makefile.include
 make veryclean
 make all
--- packages/vasp/6.2.1/compile.crane.nvhpc21.bash
+++ packages/vasp/6.2.1/compile.crane.nvhpc21.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+module purge
+module load nvhpc/21.9
+
 rm -f makefile.include && cp makefile_acc.include makefile.include
 make veryclean
 make all
