8d4c8669fc03e0aaaae00a40dea53eaf849078ac
Adam Caprez <acaprez2@unl.edu>
2022-02-01 20:37:03 -0600

    api v2 option needed for VASP.

--- packages/dftd4/3.3.0/compile.crane.gcc10.bash
+++ packages/dftd4/3.3.0/compile.crane.gcc10.bash
@@ -6,5 +6,5 @@ module load compiler/gcc/10 intel-mkl/19 cmake meson
 PREFIX=/util/opt/dftd4/3.3.0/gcc/10
 
 rm -rf _build_gcc10
-meson setup _build_gcc10 --prefix=$PREFIX -Dlapack=mkl
+meson setup _build_gcc10 --prefix=$PREFIX -Dlapack=mkl -Dapi_v2=true
 meson install -C _build_gcc10
--- packages/dftd4/3.3.0/compile.crane.intel19.bash
+++ packages/dftd4/3.3.0/compile.crane.intel19.bash
@@ -6,5 +6,5 @@ module load compiler/intel/19 intel-mkl/19 cmake meson
 PREFIX=/util/opt/dftd4/3.3.0/intel/19.0.8
 
 rm -rf _build_intel19
-meson setup _build_intel19 --prefix=$PREFIX -Dlapack=mkl
+meson setup _build_intel19 --prefix=$PREFIX -Dlapack=mkl -Dapi_v2=true
 meson install -C _build_intel19
