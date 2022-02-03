4a89e478f865d07b1510ad34ea478da5f27d651d
Adam Caprez <acaprez2@unl.edu>
2022-02-02 18:23:17 -0600

    Add scripts for VASP 6.3.0.

---
+++ packages/vasp/6.3.0/compile.crane.intel19_ompi40.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module purge
+module load compiler/intel/19 openmpi/4.0 intel-mkl/19 hdf5/1.12 wannier90/3.1 libbeef/0.1 libxc/5.1 dftd4/3.3
+
+# RHS vars are provided by the various modules
+export HDF5_ROOT=$HDF5
+export WANNIER90_ROOT=$WANNIER90
+export LIBBEEF_ROOT=$LIBBEEF
+export LIBXC_ROOT=$LIBXC
+export DFTD4_ROOT=$DFTD4
+
+rm -f makefile.include && cp makefile.include.intel_ompi_mkl_omp makefile.include
+make veryclean
+make all
