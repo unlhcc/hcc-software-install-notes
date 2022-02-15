8659b34504586c7df6bc54008eb16ed16edd835d
Adam Caprez <acaprez2@unl.edu>
2022-02-14 23:57:55 -0600

    Add build scripts for hdf-eos2.

---
+++ packages/hdf-eos2/2.20v1.00/compile.crane.gcc9.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# patch configure.ac and regenerate configure only once:
+# patch -p0 < configure.patch && autoreconf -ivf
+
+module purge
+module load compiler/gcc/9 szip/2.1 zlib/1.2 hdf4/4.2 libjpeg/2.0
+
+export CC=h4cc
+export CFLAGS="-fPIC -Df2cFortran $CFLAGS"
+export CXXFLAGS="-fPIC $CXXFLAGS"
+export FFLAGS="-fPIC $FFLAGS"
+export F90FLAGS="-fPIC $F90FLAGS"
+
+PREFIX=/util/opt/hdf-eos2/2.20/gcc/9
+
+make clean && make distclean
+./configure --enable-shared --prefix=$PREFIX --with-jpeg=${LIBJPEG} \
+	--with-zlib=${ZLIB} --with-hdf4=${HDF4} --with-szlib=${SZIP} --enable-install-include
+make
+make check
+make install
