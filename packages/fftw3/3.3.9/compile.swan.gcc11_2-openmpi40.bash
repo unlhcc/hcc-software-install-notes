4400658d90da3febc648606a35ad2629623e8685
Caughlin Bohn <cbohn4@unl.edu>
2022-06-07 09:51:16 -0500

    Added install notes for FFTW3.3.9

---
+++ packages/fftw3/3.3.9/compile.swan.gcc11_2-openmpi40.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+module purge
+module load compiler/gcc/11 openmpi/4.0
+make clean && make distclean
+
+PREFIX=/util/opt/fftw3/3.3.9/openmpi/4.0.7/gcc/11.2.0
+export CFLAGS="$CFLAGS -fPIC"
+
+# Build single-precision version
+./configure --prefix=$PREFIX --enable-sse2 --enable-avx --enable-float --enable-sse --enable-shared=yes --enable-mpi --enable-threads --enable-openmp
+make -j 4
+make install
+make clean && make distclean
+
+# Build double-precision version
+./configure --prefix=$PREFIX --enable-sse2 --enable-avx --enable-shared=yes --enable-mpi --enable-threads --enable-openmp
+make -j 4
+make install
+
---
+++ packages/fftw3/3.3.9/compile.swan.pgi20-openmpi4.0.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load compiler/pgi/20 openmpi/4.0
+make clean && make distclean
+export PREFIX=/util/opt/fftw3/3.3.9/openmpi/4.0.7/pgi/20.4
+export CFLAGS="$CFLAGS -fPIC"
+
+# Build single-precision version
+./configure --prefix=$PREFIX --enable-sse2 --enable-avx --enable-float --enable-sse --enable-shared=yes --enable-mpi --enable-threads --enable-openmp
+make -j 4
+make install
+make clean && make distclean
+
+# Build double-precision version
+./configure --prefix=$PREFIX --enable-sse2 --enable-avx --enable-shared=yes --enable-mpi --enable-threads --enable-openmp
+make -j 4
+make install
