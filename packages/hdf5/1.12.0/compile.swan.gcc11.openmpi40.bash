ec46960e6da2762ff26e6d67521dd777ef01974d
Adam Caprez <acaprez2@unl.edu>
2023-09-20 18:32:21 -0500

    Add Swan hdf5 build scripts.

---
+++ packages/hdf5/1.12.0/compile.swan.gcc11.openmpi40.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PREFIX=/util/opt/hdf5/1.12.0/openmpi/4.0.7/gcc/11
+
+module load compiler/gcc/11 openmpi/4.0 szip/2.1 zlib/1.2
+
+export CC=mpicc
+export CXX=mpic++
+export FC=mpif90
+
+make clean && make distclean
+
+./configure --prefix=${PREFIX} --enable-fortran --enable-parallel \
+ --with-szlib=$SZIP --with-zlib=$ZLIB --enable-build-mode=production
+
+make -j 4 && make install
