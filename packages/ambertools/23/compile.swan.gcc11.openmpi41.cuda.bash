d88680a45286fabfeba95b044e737e3e53bf5473
Mohammed Tanash <tanash@login1.swan.hcc.unl.edu>
2023-10-30 17:29:59 -0500

    Add AmberTools 23 notes with PMEMD

---
+++ packages/ambertools/23/compile.swan.gcc11.openmpi41.cuda.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+module purge
+module load compiler/gcc/11 openmpi/4.1 intel-mkl/20 fftw3/3.3 cmake/3.20 cuda/11.6 autotools/1.0 python/3.9 flex/2.6
+
+mkdir -p /util/opt/ambertools/23/gcc/11/openmpi/4.1
+
+rm -rf build_gpu_mpi && mkdir build_gpu_mpi && pushd build_gpu_mpi
+
+export PREFIX=/util/opt/ambertools/23/gcc/11/openmpi/4.1
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCOMPILER=GNU \
+  -DMPI=TRUE \
+  -DOPENMP=TRUE \
+  -DCUDA=TRUE \
+  -DMPI_C_COMPILER=mpicc \
+  -DMPI_CXX_COMPILER=mpic++ \
+  -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} \
+  -DDOWNLOAD_MINICONDA=FALSE \
+  -DINSTALL_TESTS=TRUE \
+  ..
+
+make -j 4
+#make install
+
