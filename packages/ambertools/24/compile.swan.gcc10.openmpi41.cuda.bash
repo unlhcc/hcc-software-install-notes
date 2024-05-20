926aac3851348db0c8fc2677fa3c642aac9f389a
Mohammed Tanash <tanash@login1.swan.hcc.unl.edu>
2024-05-20 16:54:24 -0500

    Add notes for AmberTools 24 application

---
+++ packages/ambertools/24/compile.swan.gcc10.openmpi41.cuda.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+module purge
+module load compiler/gcc/10 openmpi/4.1 intel-mkl/20 fftw3/3.3 cmake/3.20 cuda/11.6 autotools/1.0 python/3.9 flex/2.6
+
+mkdir -p /util/opt/ambertools/24/gcc/10/openmpi/4.1
+
+rm -rf build_gpu_mpi && mkdir build_gpu_mpi && pushd build_gpu_mpi
+
+export PREFIX=/util/opt/ambertools/24/gcc/10/openmpi/4.1
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCOMPILER=GNU \
+  -DMPI=TRUE \
+  -DOPENMP=TRUE \
+  -DCUDA=TRUE \
+  -DMPI_C_COMPILER=mpicc \
+  -DMPI_CXX_COMPILER=mpic++ \
+  -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} \
+  -DDOWNLOAD_MINICONDA=FALSE \
+  -DINSTALL_TESTS=TRUE \
+  ..
+
+make -j 4
+#make install
+
+patchelf --set-rpath /util/comp/gcc/10/lib64:/util/opt/ambertools/24/gcc/10/openmpi/4.1//lib:/util/opt/anaconda/deployed-conda-envs/packages/cmake/envs/cmake-3.20.2/lib /util/opt/ambertools/24/gcc/10/openmpi/4.1/bin/metatwist
+
+patchelf --set-rpath /util/comp/gcc/10/lib64:/util/opt/ambertools/24/gcc/10/openmpi/4.1//lib:/util/opt/anaconda/deployed-conda-envs/packages/cmake/envs/cmake-3.20.2/lib /util/opt/ambertools/24/gcc/10/openmpi/4.1/bin/cpptraj.OMP
+
+patchelf --set-rpath /util/comp/gcc/10/lib64:/util/opt/ambertools/24/gcc/10/openmpi/4.1//lib:/util/opt/anaconda/deployed-conda-envs/packages/cmake/envs/cmake-3.20.2/lib /util/opt/ambertools/24/gcc/10/openmpi/4.1/bin/cpptraj.cuda
+
+patchelf --set-rpath /util/comp/gcc/10/lib64:/util/opt/ambertools/24/gcc/10/openmpi/4.1//lib:/util/opt/anaconda/deployed-conda-envs/packages/cmake/envs/cmake-3.20.2/lib /util/opt/ambertools/24/gcc/10/openmpi/4.1/bin/cpptraj
+
+patchelf --set-rpath /util/comp/gcc/10/lib64:/util/opt/ambertools/24/gcc/10/openmpi/4.1//lib:/util/opt/anaconda/deployed-conda-envs/packages/cmake/envs/cmake-3.20.2/lib /util/opt/ambertools/24/gcc/10/openmpi/4.1/bin/ambpdb
