816bbb383bbd93432fa91aca4b360ac2d0284020
Natasha Pavlovikj <npavlovikj@login1.swan.hcc.unl.edu>
2024-07-16 17:00:54 -0500

    Add some install notes

---
+++ packages/nvidia-hpc-sdk/2024.245/compile.swan.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module purge
+
+# cd /util/src/nvidia-hpc-sdk
+# wget https://developer.download.nvidia.com/hpc-sdk/24.5/nvhpc_2024_245_Linux_x86_64_cuda_12.4.tar.gz
+# tar xvf nvhpc_2024_245_Linux_x86_64_cuda_12.4.tar.gz
+nvhpc_2024_245_Linux_x86_64_cuda_12.4/install
+
+# Prompts:
+# Enter
+# 1) Single system install
+# 2) /util/opt/nvidia-hpc-sdk/2024.245/
