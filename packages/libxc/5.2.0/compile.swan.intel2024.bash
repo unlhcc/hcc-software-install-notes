006be378141031b1ec85619019d3cd4cf8d41df1
Mohammed Tanash <tanash@login1.swan.hcc.unl.edu>
2025-05-07 17:40:25 -0500

    Add software install notes for libxc-5.1.6 and libxc-5.2 built with compiler Intel2024

---
+++ packages/libxc/5.2.0/compile.swan.intel2024.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+  
+module purge
+module load compiler/intel/2024
+
+export CC=icx
+export FC=ifx
+export F77=ifx
+export FCFLAGS="-march=haswell -mtune=icelake-server"
+export LDFLAGS="-lifcoremt -lifport -limf -lsvml -lirc -lirc_s -lpthread -ldl"
+
+# Clean previous attempts
+make clean
+make distclean
+
+
+# Run configure manually setting FCLIBS (not just LDFLAGS)
+./configure \
+  CC=icx \
+  FC=ifx \
+  F77=ifx \
+  FCLIBS="$LDFLAGS" \
+  --prefix=/util/opt/libxc/5.2.0/intel/2024 \
+  --enable-shared
+
+make -j 4
+make install
